ebf89208b508872ac08c40bd204d4780b54c2e9e
Gustavo Loyola <guga.loyola17@gmail.com>
2023-08-12 21:37:32 -0300

    ToLowerCase removido para evitar bugs

--- src/services/getPokemonData.js
+++ src/services/getPokemonData.js
@@ -1,6 +1,5 @@
-async function getPokemonData(name) {
-    const nameLowerCase = name.toLowerCase()
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${nameLowerCase}/`)
+async function getPokemonData(id) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
     const pokemon = await response.json()
     return await pokemon
 }
