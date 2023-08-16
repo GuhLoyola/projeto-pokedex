73baa61920f13483007c05008e0e12288076d0f8
Gustavo Loyola <guga.loyola17@gmail.com>
2023-08-16 01:49:27 -0300

    Página solo do pokemon criada

--- src/components/pokemon-page/pokemon-page.js
+++ src/components/pokemon-page/pokemon-page.js
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom"
 import { getPokemonData } from "../../services/getPokemonData"
 import { Header } from "../header/header"
 import { ReactComponent as Load } from '../../assets/load.svg'
+import { PokemonAbilities } from "../pokemon-abilities/pokemon-abilities"
 
 const PokemonPage = ({ load, props }) => {
 
@@ -24,13 +25,52 @@ const PokemonPage = ({ load, props }) => {
             <Header />
             <section>
                 <div>
-                    {/* {load? <Load/> : 
-                        pokemon?
+                    {load ? <Load /> :
+                        pokemon ?
                             <>
                                 <h2>{pokemon.name}</h2>
-                                <img src={pokemon.sprites?.other["official-artwork"].front_default}/>
+                                <p>id #{pokemon.id}</p>
+                                <img alt={pokemon.name} src={pokemon.sprites?.other["official-artwork"].front_default} />
+                                <div>
+                                    <h3>Types</h3>
+                                    <ul>
+                                        {pokemon.types?.map((element, index) => {
+                                            return (
+                                                <li key={index}>
+                                                    {element.type?.name}
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                </div>
+                                <div>
+                                    <h3>Abilities</h3>
+                                    <ul>
+                                        {pokemon.abilities?.slice(0, 5).map((element, index) => {
+                                            return (
+                                                <li key={index}>
+                                                    <PokemonAbilities abilitiesUrl={element.ability?.url} />
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                </div>
+                                <div>
+                                    <h3>Moves</h3>
+                                    <ul>
+                                        {pokemon.moves?.slice().map((element, index) => {
+                                            return (
+                                                <li key={index}>
+                                                    {element.move?.move}
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                </div>
                             </>
-                } */}
+                            :
+                            <p>Sorry...Pokemon not found 😥</p>
+                    }
                 </div>
             </section>
         </>
