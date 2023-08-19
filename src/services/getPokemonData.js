440eab53894d734c47c30d338d664c8627a35e1f
Gustavo Loyola <guga.loyola17@gmail.com>
2023-08-19 17:18:55 -0300

    função 'toLowerCase' adicionado ao service para evitar bugs

--- src/services/getPokemonData.js
+++ src/services/getPokemonData.js
@@ -1,5 +1,6 @@
 async function getPokemonData(id) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    const fetchedId = id.toLowerCase()
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${fetchedId}/`)
     const pokemon = await response.json()
     return await pokemon
 }
